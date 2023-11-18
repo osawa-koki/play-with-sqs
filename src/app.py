db68c8268baea8e5b03ed88ea246575826bda83b
osawa-koki <koki@osawa.dev>
2023-11-19 00:50:23 +0900

    キューイングAPIの実装。

--- src/app.py
+++ src/app.py
@@ -1,11 +1,27 @@
 import os
 
+import boto3
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from pydantic import BaseModel
 from mangum import Mangum
 
 app = FastAPI()
 load_dotenv()
+
+client = boto3.client('sqs')
+
+QUEUE_URL = os.environ.get('QUEUE_URL')
+
+
+class EnqueueModel(BaseModel):
+    message_group_id: str
+    data: list[str]
+
+
+class DequeueModel(BaseModel):
+    message_group_id: str
+    max_number_of_messages: int = 1
 
 
 @app.get("/api/ping")
@@ -16,6 +32,35 @@ def read_root():
 @app.get("/api/envs")
 def read_envs():
     return {"envs": os.environ}
+
+
+@app.post("/api/enqueue")
+def enqueue(model: EnqueueModel):
+    for data in model.data:
+        client.send_message(
+            QueueUrl=QUEUE_URL,
+            MessageGroupId=model.message_group_id,
+            MessageBody=data,
+        )
+    return {"message": "Enqueued"}
+
+
+@app.post("/api/dequeue")
+def dequeue(model: DequeueModel):
+    result = []
+    response = client.receive_message(
+        QueueUrl=QUEUE_URL,
+        MaxNumberOfMessages=model.max_number_of_messages,
+    )
+    messages = response.get('Messages')
+    if messages:
+        for message in messages:
+            result.append(message.get('Body'))
+            client.delete_message(
+                QueueUrl=QUEUE_URL,
+                ReceiptHandle=message.get('ReceiptHandle')
+            )
+    return {"message": result}
 
 
 lambda_handler = Mangum(app)
