bd2520eac1781c79c074370e92e771626fa3ce60
osawa-koki <koki@osawa.dev>
2023-11-18 23:45:57 +0900

    環境変数一覧の取得。

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from mangum import Mangum
 
@@ -7,6 +9,11 @@ app = FastAPI()
 @app.get("/api/ping")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/envs")
+def read_envs():
+    return {"envs": os.environ}
 
 
 lambda_handler = Mangum(app)
